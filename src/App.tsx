e82d349729f20f4cea340fc1994c86746054f0c7
Koki Takahashi <hakatasiloving@gmail.com>
2022-11-06 00:43:43 +0900

    Remove svg assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import {useState} from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import ReactPlayer from 'react-player';
 import songs from '../data/songs.yml';
@@ -11,14 +10,6 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src="/vite.svg" className="logo" alt="Vite logo"/>
-				</a>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo"/>
-				</a>
-			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button type="button" onClick={() => setCount((oldCount) => oldCount + 1)}>
