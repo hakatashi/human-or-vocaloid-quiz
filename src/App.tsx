2c44b37da10b75686a8dc957d4295fd8220ff9ce
Koki Takahashi <hakatasiloving@gmail.com>
2022-11-06 02:02:23 +0900

    Switch to CSS modules mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import {useState} from 'react';
-import './App.css';
 import ReactPlayer from 'react-player';
 import songs from '../data/songs.yml';
+import style from './App.module.css';
 
 const App = () => {
 	const [count, setCount] = useState(0);
 	return (
-		<div className="App">
+		<div className={style.root}>
 			<h1>Vite + React</h1>
-			<div className="card">
+			<div className={style.card}>
 				<button type="button" onClick={() => setCount((oldCount) => oldCount + 1)}>
 					count is {count}
 				</button>
