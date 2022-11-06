e3cd1220c3908fbd0b5139bf3379eea7a88adbce
Koki Takahashi <hakatasiloving@gmail.com>
2022-11-06 23:01:47 +0900

    Implement answer description section

--- src/Game.tsx
+++ src/Game.tsx
@@ -41,35 +41,80 @@ const Game = ({tick, index}: Prop) => {
 
 	return (
 		<div>
-			<h2>第{index}/10問</h2>
-			<p>この歌声、<span className="human">人間？</span> <span className="vocaloid">ボカロ？</span></p>
-			<div className={style.options}>
-				<button
-					type="button"
-					className={style.option}
-					onClick={() => onClickOption(true)}
-				>
-					<span className={classNames(style.optionName, 'human')}>
-						人間
-					</span><br/>
-					<span className={style.description}>
-						歌い手・VTuberなどの<wbr/>
-						肉声をそのまま録音した歌声
-					</span>
-				</button>
-				<button
-					type="button"
-					className={style.option}
-					onClick={() => onClickOption(false)}
-				>
-					<span className={classNames(style.optionName, 'vocaloid')}>
-						ボカロ
-					</span><br/>
-					<span className={style.description}>
-						Vocaloid・SynthV・UTAUなど、<wbr/>
-						合成音声による歌声
-					</span>
-				</button>
+			<div className={style.descriptionSection}>
+				<div className={style.questionSection}>
+					<h2>第{index}/10問</h2>
+					<p>この歌声、<span className="human">人間？</span> <span className="vocaloid">ボカロ？</span></p>
+					<div className={style.options}>
+						<button
+							type="button"
+							className={style.option}
+							onClick={() => onClickOption(true)}
+						>
+							<span className={classNames(style.optionName, 'human')}>
+								人間
+							</span><br/>
+							<span className={style.description}>
+								歌い手・VTuberなどの<wbr/>
+								肉声をそのまま録音した歌声
+							</span>
+						</button>
+						<button
+							type="button"
+							className={style.option}
+							onClick={() => onClickOption(false)}
+						>
+							<span className={classNames(style.optionName, 'vocaloid')}>
+								ボカロ
+							</span><br/>
+							<span className={style.description}>
+								Vocaloid・SynthV・UTAUなど、<wbr/>
+								合成音声による歌声
+							</span>
+						</button>
+					</div>
+				</div>
+				{selectedOption !== null && (
+					<div className={style.answerSection}>
+						<h2>正解！</h2>
+						<p className={style.answerDescription}>
+							この歌声は
+							{song.isHuman ? <span className="human">人間</span> : <span className="vocaloid">合成音声</span>}
+							です。
+						</p>
+						<div className={style.row}>
+							<table className={style.songInfo}>
+								<tr>
+									<th>楽曲</th>
+									<td>{song.title} by {song.artist}</td>
+								</tr>
+								<tr>
+									<th>シンガー</th>
+									<td>{song.singer}</td>
+								</tr>
+							</table>
+						</div>
+						<div className={style.row}>
+							<a
+								href={song.url}
+								className={style.youtubeLink}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								YouTubeで観る
+							</a>
+						</div>
+						<div className={style.row}>
+							<button
+								type="button"
+								className={style.nextQuestion}
+								onClick={() => onClickOption(false)}
+							>
+								次の問題へ
+							</button>
+						</div>
+					</div>
+				)}
 			</div>
 			<div className={style.player}>
 				<div className={classNames(style.playerOverlay, {[style.hidden]: selectedOption !== null})}>
